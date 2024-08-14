5a466d14a8634f0dfc9979845d1c95fb0b963ed8
ChaimEzra <chimas1357@gmail.com>
2024-08-14 12:22:20 +0300

    commit

---
+++ Lesson1/targil1.py
@@ -0,0 +1,20 @@
+
+
+a= 12
+b=11.56
+name="chaim Ezra"
+print(a)
+print(b)
+print(name)
+
+# this is a comment
+print(type(a))
+print(type(b))
+print(type(name))
+
+print("Hello world\n How are you?")
+print("Hello"+ name + " " +str(12))
+
+mail=input("Enter your email: ")
+print(mail)
+print(type(mail))
---
+++ Lesson1/targil2.py
@@ -0,0 +1,25 @@
+# num = 5
+# num += 2
+# print(num)
+
+# num = 3
+# num = num ** 2
+# print(num ** 2)
+'''
+Alafim = 4
+Meot = 5
+Asarot = 6
+Ahadot = 7
+'''
+num4 = 8888
+Ahadot = num4 % 10
+num3 = num4 // 10
+Asarot = num3 % 10
+num2 = num3 // 10
+Meot = num2 % 10
+num1 = num2 // 10
+Alafim = num1 % 10
+print("Alafim = " + str(Alafim))
+print("Meot = " + str(Meot))
+print("Asarot = " + str(Asarot))
+print("Ahadot = " + str(Ahadot))
