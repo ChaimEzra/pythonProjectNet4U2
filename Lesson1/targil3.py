d35f0226e4ad26b476299b9ee7b95e48e6593e64
ChaimEzra <chimas1357@gmail.com>
2024-08-15 18:11:20 +0300

    commit

---
+++ Lesson1/targil3.py
@@ -0,0 +1,5 @@
+newList=[2,3,4,"chaim",[]]
+
+print(type(newList))
+print(newList)
+
---
+++ Lesson1/targil4.py
@@ -0,0 +1,22 @@
+# filepath = "C:/Users/chaim/OneDrive/שולחן העבודה/commends linox.txt"
+# file = open(filepath, "r")
+# # for line in file:
+# #     print(line)
+# print(file.read())
+# file.close()
+
+# print(file.read())
+# file.write("192.168.1.1\n192.168.1.2")
+# print(file.read())
+# file.close()
+
+# file = open(filepath, "a")
+# file.write("192.168.1.3 \n 192.168.1.4")
+# file.close()
+#
+# file = open(filepath, "r")
+# print(file.read())
+# file.close()
+
+for i in range(1,11):
+    print(i)
